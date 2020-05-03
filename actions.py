6a615fbc754b85afdf7508458bc16b38aeaee9ae
ovidiunegru <44364275+ovidiunegru@users.noreply.github.com>
2020-05-03 22:43:50 +0300

    Connection to database created and returns the data from exercice search provider
    At the current state the databse responds to the call and returns the exericices asked for but not in a beautiful way

--- actions.py
+++ actions.py
@@ -12,6 +12,9 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+from rasa_sdk.forms import FormAction
+import mysql.connector
+
 
 class ActionExerciceSearch(Action):
 
@@ -21,11 +24,35 @@ class ActionExerciceSearch(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="Chifteabobo0228",
+            database="testdatabase"
+            #auth_plugin='mysql_native_password'
+        )
 
         exercice = tracker.get_slot("exercice_type")
         muscularGroup = tracker.get_slot("muscular_group")
-        dispatcher.utter_message("Here is the muscle group of the {}:{}".format(exercice,muscularGroup))
 
+        cursor = mydb.cursor()
+        query="SELECT * FROM Exercice where exercice_group = '%s'" % (muscularGroup)
+        try:
+            cursor.execute(query)
+            if cursor.execute(query) == 0:
+                print("Sorry There was an issue")
+        except:
+            print("Error 1: unable to retreive data")
+
+        #dispatcher.utter_message("Here is the muscle group of the {}:{}".format(exercice,muscularGroup))
+        dispatcher.utter_message("Here are some exercices for {}".format(muscularGroup))
+
+        records = cursor.fetchall()
+
+        for exercices in records:
+            exerciceList = exercices[0],
+
+        dispatcher.utter_message(str(exerciceList))
         return []
 
 
@@ -38,6 +65,8 @@ class ActionHelloWorldCustom(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Hello World! First custom action")
+        dispatcher.utter_message(text="Hello World! SECOND custom action")
 
         return []
+
+
